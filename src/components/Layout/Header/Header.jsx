c219370b2528095a7092d08c3d4c6973169a49c2
dincerdegre <dincerdegre@gmail.com>
2023-05-31 15:36:29 +0300

    changed header structure for responsive integ

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.scss";
 import logo from "../../../assets/images/logo.png";
+import Hamburger from "../Hamburger/Hamburger";
+import Menu from "../Menu/Menu";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const openMenuHandler = () => {
+    setOpenMenu((prev) => !prev);
+  };
+
   return (
     <header>
       <div className={`${styles.headerContainer} row flex`}>
@@ -14,13 +22,23 @@ const Header = () => {
         </div>
         <div className={styles.nav}>
           <ul>
-            <li><Link to={"/"}>Burslu Çanta</Link></li>
-            <li><Link to={"/"}>Clutch</Link></li>
-            <li><Link to={"/"}>Jut Çanta</Link></li>
-            <li><Link to={"/"}>Şapka</Link></li>
+            <li>
+              <Link to={"/"}>Burslu Çanta</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Clutch</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Jut Çanta</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Şapka</Link>
+            </li>
           </ul>
         </div>
+        <Hamburger className={styles.hamburger} onClick={openMenuHandler} />
       </div>
+      <Menu openMenu={openMenu} onClick={openMenuHandler} />
     </header>
   );
 };
